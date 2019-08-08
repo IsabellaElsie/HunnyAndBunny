2308f1474be1b84764d55c184ec1b75d47d1d7a1
Isabella Clutterham <isabella.elsie.shaw@gmail.com>
2019-08-08 17:58:34 +0930

    Initial Commit

---
+++ Assets/Scripts/MonoBehaviour/Camera/InteractionManagement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManagement : MonoBehaviour
+{
+    public Interactable currentObject;
+
+    LayerMask m_LayerMask;
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+
+
+    private void Awake()
+    {
+        m_LayerMask = LayerMask.GetMask("InteractableItems");
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, m_LayerMask))
+        {
+            /*IF hit object == An Interactable Object*/
+               // Debug.Log("RayCast HIT!");
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        Debug.Log("OnMouseEnter called.");
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        Debug.Log("OnMouseExit called.");
+    }
+
+}
---
+++ Assets/Scripts/MonoBehaviour/Interactable/Interactable.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interactable : MonoBehaviour// , IPointerEnterHandler, IPointerExitHandler
+{
+    //public Transform InteractionLocation;
+
+    public void RunInteraction()
+    {
+        Debug.Log("Interaction is running.");
+    }
+
+
+ 
+
+    /*public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Enter.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Exit.");
+    }*/
+
+
+}
